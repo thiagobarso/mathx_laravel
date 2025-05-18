04bb91e16a5b1fa01adfc6501289f8ab756017aa
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-05-18 15:34:21 -0300

    107. Optimizar Código e Preservar Dados em Sessão

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -50,52 +50,15 @@ class MainController extends Controller
         // generate exercises
         $exercises = [];
         for ($index = 1; $index <= $numberExercises; $index++) {
-
-            $operation = $operations[array_rand($operations)];
-            $number1 = rand($min, $max);
-            $number2 = rand($min, $max);
-
-            $exercise = '';
-            $sollution = '';
-
-            switch ($operation) {
-                case 'sum':
-                    $exercise = "$number1 + $number2 =";
-                    $sollution = $number1 + $number2;
-                    break;
-                case 'subtraction':
-                    $exercise = "$number1 - $number2 =";
-                    $sollution = $number1 - $number2;
-                    break;
-                case 'multiplication':
-                    $exercise = "$number1 x $number2 =";
-                    $sollution = $number1 * $number2;
-                    break;
-                case 'division':
-
-                    // avoid division by zero
-                    if ($number2 == 0) {
-                        $number2 = 1;
-                    }
-                    
-                    $exercise = "$number1 : $number2 =";
-                    $sollution = $number1 / $number2;
-                    break;
-            }
-
-            // if $sollution is a float number, round it to 2 decimal places
-            if (is_float($sollution)) {
-                $sollution = round($sollution, 2);
-            }
-
-            $exercises[] = [
-                'operation' => $operation,
-                'exercise_number' => $index,
-                'exercise' => $exercise,
-                'sollution' => "$exercise $sollution"
-            ];
+            $exercises[] = $this->generateExercise($index, $operations, $min, $max);
         };
-        
+
+        // place exercises in session
+        // method 1
+        // $request->session()->put('exercises', $exercises);
+        // or method 2
+        session(['exercises' => $exercises]);
+
         return view('operations', ['exercises' => $exercises]);
     }
 
@@ -107,5 +70,52 @@ class MainController extends Controller
     public function exportExercises()
     {
         echo 'exportar exercícios para um arquivo de texto';
+    }
+
+    private function generateExercise($index, $operations, $min, $max): array
+    {
+        $operation = $operations[array_rand($operations)];
+        $number1 = rand($min, $max);
+        $number2 = rand($min, $max);
+
+        $exercise = '';
+        $sollution = '';
+
+        switch ($operation) {
+            case 'sum':
+                $exercise = "$number1 + $number2 =";
+                $sollution = $number1 + $number2;
+                break;
+            case 'subtraction':
+                $exercise = "$number1 - $number2 =";
+                $sollution = $number1 - $number2;
+                break;
+            case 'multiplication':
+                $exercise = "$number1 x $number2 =";
+                $sollution = $number1 * $number2;
+                break;
+            case 'division':
+
+                // avoid division by zero
+                if ($number2 == 0) {
+                    $number2 = 1;
+                }
+
+                $exercise = "$number1 : $number2 =";
+                $sollution = $number1 / $number2;
+                break;
+        }
+
+        // if $sollution is a float number, round it to 2 decimal places
+        if (is_float($sollution)) {
+            $sollution = round($sollution, 2);
+        }
+
+        return [
+            'operation' => $operation,
+            'exercise_number' => $index,
+            'exercise' => $exercise,
+            'sollution' => "$exercise $sollution"
+        ];
     }
 }
