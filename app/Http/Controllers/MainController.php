6fc4bdda57a32777a64e2a2819d6d10e35984daa
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-05-18 14:33:14 -0300

    102.Validação do Formulário

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,7 +14,18 @@ class MainController extends Controller
 
     public function generateExercises(Request $request)
     {
-        echo 'gerar exercicios';
+        $request->validate([
+            'check_sum' => 'required_without_all::check_subtraction, check_multiplication, check_division',
+            'check_subtraction' => 'required_without_all::check_sum, check_multiplication, check_division',
+            'check_multiplication' => 'required_without_all::check_sum, check_subtraction, check_division',
+            'check_division' => 'required_without_all::check_sum, check_subtraction, check_multiplication',
+            'number_one' => 'required|integer|min:0|max:999',
+            'number_two' => 'required|integer|min:0|max:999',
+            'number_exercises' => 'required|integer|min:5|max:50',
+        ]);
+
+        dd($request->all());
+       
     }
 
     public function printExercises()
